5d78e043a1cdb6fb7e85e1dc24b00106474be0ce
sieep-coding <nickstambaugh@proton.me>
2025-11-28 18:06:03 -0500

    update

--- docs/docs/astro.config.mjs
+++ docs/docs/astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeGalaxy from 'starlight-theme-galaxy'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			plugins: [starlightThemeGalaxy()],
 			title: 'LunivCore',
+			customCss: [
+				'./src/styles/custom.css',
+			],
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/sieep-coding/lunivcore' }
 
